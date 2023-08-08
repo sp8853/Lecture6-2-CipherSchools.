cb2171d51d91caa91ff387cdfab869fe461e5f0d
sp8853 <103991911+sp8853@users.noreply.github.com>
2023-08-08 23:21:36 +0530

    Add files via upload

---
+++ lec6-3.cpp
@@ -0,0 +1,15 @@
+"WHILE LOOP"
+while(condition)
+{
+ 
+int i=1;
+while(i<=5)
+{
+std::cout<<i<<"";
+i++;
+}
+
+* break statement and why is it important 
+suppose run a code untill the user inputs a negative number 
+sometimes we dont know when to end the loop without actually running the loop so we have to stop the loop somewhere in the middle , this is where the "break"
+statement comes into picture
